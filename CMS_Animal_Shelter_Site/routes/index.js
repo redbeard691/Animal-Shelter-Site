a9bafb9ba186fbb4a8ee4f590fbed51f91a0fe7d
Rick Roslof <rick.roslof@gmail.com>
2025-04-14 20:42:17 -0700

    Implemented Lost & Found pet listings on homepage. Removed search button from header.

--- CMS_Animal_Shelter_Site/routes/index.js
+++ CMS_Animal_Shelter_Site/routes/index.js
@@ -1,8 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var { Post } = require('../model/Post')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+  const lostPosts = await Post.findAll(
+    {
+        where: {
+          status: "Lost"
+        },
+        order: [
+            [ "createdAt", "DESC" ]
+        ]
+    }
+  )
+
+  const foundPosts = await Post.findAll(
+    {
+        where: {
+          status: "Found"
+        },
+        order: [
+            [ "createdAt", "DESC" ]
+        ]
+    }
+  )
+
+  // TODO: Query bulletins
+
+  res.locals.lostPosts = lostPosts
+  res.locals.foundPosts = foundPosts
   res.render('index');
 });
 
